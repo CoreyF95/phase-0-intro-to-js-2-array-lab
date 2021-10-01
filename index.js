2df9175a783bff7a281d5d16b9870f42409c2f06
Corey Fisher <coreyfisher2021@gmail.com>
2021-10-01 13:33:45 -0500

    commit tree ed99b63731f35e050a75db1314f50d74c457aeed

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
-const cats = ["Milo", "Otis", "Garfield"];
+let cats = [
+  "Milo",
+  "Otis",
+  "Garfield"
+];
 
-function destructivelyAppendCat() {
-  cats.push("Ralph");
+function destructivelyAppendCat(name) {
+  cats.push(name);
 }
 
-function destructivelyPrependCat(){
-  cats.unshift("Bob");
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
 }
 
 function destructivelyRemoveLastCat() {
-  cats.splice(2,3);
+  cats.pop();
 }
 
 function destructivelyRemoveFirstCat() {
-  cats.splice(0,1);
+  cats.shift();
 }
 
 function appendCat(name) {
-  return [...cats, name];
+  const copyCats = [...cats];
+  copyCats.push(name);
+  return copyCats;
 }
 
 function prependCat(name) {
-  return [name, ...cats];
+  const copyCats = [...cats];
+  copyCats.unshift(name);
+  return copyCats;
 }
 
-function removeLastCat() {
-  return cats.slice(0,cats.length-1);
+function removeLastCat(name) {
+  const copyCats = [...cats];
+  copyCats.pop(name);
+  return copyCats;
 }
 
-function removeFirstCat() {
-  return cats.slice(1);
+function removeFirstCat(name) {
+  const copyCats = [...cats];
+  copyCats.shift(name);
+  return copyCats;
 }
